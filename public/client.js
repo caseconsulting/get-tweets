b3e44b345551edb78d00ef632ce218090a64e2d7
Fahad <fahada95@vt.edu>
2017-06-13 08:58:08 -0400

    express-basics: added basic express functionality

---
+++ public/client.js
@@ -0,0 +1,17 @@
+
+
+//test function
+function helloWorld()
+{
+  console.log("I am your father");
+}
+
+//tweet button click
+$( "#tweets" ).click(function() {
+  //outgoing request for tweets
+  $.get('/tweets', function(req,res, body){
+    //write to the doc
+  document.write(body);
+  });
+
+});
