b2a595c32c8cf696de476e2d21e659cf48f66409
jacob <jacobboisseau@gmail.com>
2017-06-13 11:12:00 -0400

    adding-tweet-json: got text input as variable

--- public/client.js
+++ public/client.js
@@ -9,9 +9,13 @@ function helloWorld()
 //tweet button click
 $( "#tweets" ).click(function() {
   //outgoing request for tweets
+  var input = document.getElementById("input").value;
+  console.log(input);
+  body = input;
   $.get('/tweets', function(req,res, body){
     //write to the doc
-    document.write(req);
+
+    //document.write(req);
   });
 
 });
