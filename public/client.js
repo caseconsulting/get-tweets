d8bc16554ea57e117092bd35cf2c8fab77872276
Fahad <fahada95@vt.edu>
2017-06-13 11:13:51 -0400

    express-basics: added json output to page through client-side jquery

--- public/client.js
+++ public/client.js
@@ -52,8 +52,16 @@ $( "#tweets" ).click(function() {
 >>>>>>> adding-tweet-json: got text input as variable
   $.get('/tweets', function(req,res, body){
     //write to the doc
+<<<<<<< HEAD
 
     //document.write(req);
+=======
+    for (let key in req)
+    {
+      $('#tweetCallback').append('<p>' + req[key].text + '<p>');
+    }
+
+>>>>>>> express-basics: added json output to page through client-side jquery
   });
 
 });
