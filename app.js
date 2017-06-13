b94e255ab53b213c3706bcec6c905fb5bdb33658
jacob <jacobboisseau@gmail.com>
2017-06-13 09:53:39 -0400

    adding-tweet-json: moved tweet getting into app.js for now to test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const tweet = require('./lib/tweets');
 
 //serves everything in the public folder. ie. index.html and client.js
 app.use(express.static('public'));
@@ -7,8 +8,27 @@ app.use(express.static('public'));
 //route for tweets called by tweets function
 app.get('/tweets', function (req, res, body) {
   //send json file
-  body = "hello skywalker";
-  res.send(body);
+  'use strict';
+
+  var Twitter = require('twitter');
+  require('dotenv').config();
+
+  var params = {screen_name: "boisseaujacob",exclude_replies:true,include_rts:false};
+
+  var client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  });
+
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        res.send(JSON.stringify(tweets, undefined, 4));
+      }
+    });
+
 });
 
 //initialize the server on port 3000
