dd8d29a10d91920b4342a4c42fa12cf9947b184f
jacob <jacobboisseau@gmail.com>
2017-06-14 10:01:57 -0400

    adding-friends: called new format in app.js from middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var twitter = require('./twitterapi');
-var tweetsmiddle = require('./twittermiddleware');
+var twittermiddleware = require('./twittermiddleware');
 //serves everything in the public folder. ie. index.html and client.js
 app.use(express.static('public'));
 
 //route for tweets called by tweets function
-app.get('/tweets', tweetsmiddle);
+app.get('/tweets', twittermiddleware.tweets);
 
 //initialize the server on port 3000
 app.listen(3000, function()
