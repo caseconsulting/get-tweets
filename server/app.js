a79975b2b0c8a2a95b2d20159602b8a4f0992342
Fahad <fahada95@vt.edu>
2017-06-14 09:47:28 -0400

    add-routes: refactored app.js

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
 var twitter = require('./twitterapi');
-var tweetsmiddle = require('./twittermiddleware');
+var middleware = require('./twittermiddleware');
 //serves everything in the public folder. ie. index.html and client.js
 app.use(express.static('public'));
 
-//route for tweets 
-app.get('/tweets', tweetsmiddle);
+//route for tweets
+app.get('/tweets', middleware.tweets);
 
 //route for friends
-app.get('/friends', friendsmiddle);
+app.get('/friends', middleware.friends);
 
 //route for followers
-app.get('/followers', followersmiddle);
+app.get('/followers', middleware.followers);
 
 //initialize the server on port 3000
 app.listen(3000, function()
