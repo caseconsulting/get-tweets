33c3e9950a7f43e23daf58ab8b75e84e71202819
Fahad <fahada95@vt.edu>
2017-06-14 09:53:17 -0400

    add-routes: added functionality for followers and friends button

--- public/client.js
+++ public/client.js
@@ -14,3 +14,38 @@ $( "#tweets" ).click(function() {
       }
     });
   });
+
+
+  //tweet button click
+  $( "#friends" ).click(function() {
+    //outgoing request for tweets
+
+    var input = document.getElementById("input").value;
+
+    console.log("Username sent: "+input);
+
+    $.get('/tweets', {handle: input + ''}, function(req, res, body){
+        for (let key in req)
+        {
+          $('#tweetCallback').append('<h6>' + req[key].text + '<h6>');
+
+        }
+      });
+    });
+
+    //tweet button click
+    $( "#followers" ).click(function() {
+      //outgoing request for tweets
+
+      var input = document.getElementById("input").value;
+
+      console.log("Username sent: "+input);
+
+      $.get('/tweets', {handle: input + ''}, function(req, res, body){
+          for (let key in req)
+          {
+            $('#tweetCallback').append('<h6>' + req[key].text + '<h6>');
+
+          }
+        });
+      });
