b8de23a3e1cc31caf9fd23fe94333d72f13e95b0
jacob <jacobboisseau@gmail.com>
2017-06-14 09:56:39 -0400

    adding-friends: added frined function in middleware

--- server/twittermiddleware.js
+++ server/twittermiddleware.js
@@ -1,6 +1,6 @@
 var twitter = require('./twitterapi');
 
-function tweetsmiddle(req, res, body) {
+function tweets(req, res, body) {
   //send json file
   'use strict';
   let username = req.query.handle;
@@ -13,4 +13,27 @@ function tweetsmiddle(req, res, body) {
 
 }
 
-module.exports = tweetsmiddle;
+function friends(req, res, body){
+  let username = req.query.handle;
+
+  if(username[0]=='@'){
+  username = username.replace('@','');
+  }
+  console.log("Username recived: "+username);
+  twitter.getfriends(username,res);
+}
+
+function followers(req,res,body){
+  let username = req.query.handle;
+
+  if(username[0]=='@'){
+  username = username.replace('@','');
+  }
+  console.log("Username recived: "+username);
+  twitter.getFollowers(username,res);
+}
+
+let twitterFunction = {
+  tweets, friends, followers
+}
+module.exports = twitterFunction;
