98a523ee2cbca3e1342c652fb80db8727aaf92c9
Fahad <fahada95@vt.edu>
2017-06-16 16:19:08 -0400

    UI-fun: added few more tests

---
+++ spec/public/client.spec.js
@@ -0,0 +1,9 @@
+//const $ = require('jasmine-jquery');
+//const client = require('../../public/client');
+xdescribe("Client specs", () => {
+  describe("Draw", () =>{
+    it("Is a function", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
